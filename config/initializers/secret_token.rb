1fad5fff6161ce2972c755bd8226b30acf454d3b
Daniel Kehoe <kehoe@fortuity.com>
2010-10-28 20:33:44 -0700

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdwordsKeywordTool::Application.config.secret_token = 'e2fb29d4a19850e41f0219f298b0adfe7b56e57bc79b1a73cc6179ed9adcb101bb91d914f91f2ce2ff9a86fbdedab23e5d4f49949ac6793fbc234540504feb17'
