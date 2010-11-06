df8f95b726660bc4ac4b5adeb68d90dd3f9b322b
Daniel Kehoe <kehoe@fortuity.com>
2010-11-06 14:42:58 -0700

    add admin role to User and modify the default user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User
   include Mongoid::Document
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   field :name
+  field :admin, :type => Boolean, :default => false
+  key :name
   validates_presence_of :name
   validates_uniqueness_of :name, :email, :case_sensitive => false
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :admin
 end
 
